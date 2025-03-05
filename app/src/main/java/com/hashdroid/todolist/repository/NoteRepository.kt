d71e310a6e51f5375a9bb8236392524b76fcbeec
Harsh Kumar Sharma <kumarsharmaharsh2004@gmail.com>
2025-03-06 00:30:54 +0530

    Room Database added

---
+++ app/src/main/java/com/hashdroid/todolist/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.hashdroid.todolist.repository
+
+import androidx.room.Query
+import com.hashdroid.todolist.database.NoteDatabase
+import com.hashdroid.todolist.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query!!)
+}
