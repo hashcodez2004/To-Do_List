f96ac82052ad6ef3f771dd368a7db8880f001ede
Harsh Kumar Sharma <kumarsharmaharsh2004@gmail.com>
2025-03-06 14:40:24 +0530

    ViewModel and Adapter added

--- app/src/main/java/com/hashdroid/todolist/MainActivity.kt
+++ app/src/main/java/com/hashdroid/todolist/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.hashdroid.todolist.database.NoteDatabase
+import com.hashdroid.todolist.repository.NoteRepository
+import com.hashdroid.todolist.viewmodel.NoteViewModel
+import com.hashdroid.todolist.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
